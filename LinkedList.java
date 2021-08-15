b3906b0570492f2dbf1f1dda1ddc27058faa9893
moshtab <87015799+moshtab@users.noreply.github.com>
2021-08-15 12:48:31 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -39,31 +39,6 @@ class LinkedList {
 		prev_node.next = newNode;
 	}
 
-	public void deleteNode(int key) {
-		// Store head node
-		Node temp = head, prev = null;
-
-		// If head node itself holds the key to be deleted
-		if (temp != null && temp.data == key) {
-			head = temp.next; // Changed head
-			return;
-		}
-
-		// Search for the key to be deleted, keep track of
-		// the previous node as we need to change temp.next
-		while (temp != null && temp.data != key) {
-			prev = temp;
-			temp = temp.next;
-		}
-
-		// If key was not present in linked list
-		if (temp == null)
-			return;
-
-		// Unlink the node from linked list
-		prev.next = temp.next;
-
-	}
 
     //Checks whether the value x is present in linked list
     public boolean search(Node head, int x)
